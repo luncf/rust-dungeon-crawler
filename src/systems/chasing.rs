40ddf181f1050af957cf813d1a776c05e7f5e2e4
Andy Lun <andy.luncf@gmail.com>
2021-11-07 20:21:09 -0600

    Add fov for player and monsters

--- src/systems/chasing.rs
+++ src/systems/chasing.rs
@@ -5,8 +5,9 @@ use crate::prelude::*;
 #[read_component(ChasingPlayer)]
 #[read_component(Health)]
 #[read_component(Player)]
+#[read_component(FieldOfView)]
 pub fn chasing(#[resource] map: &Map, ecs: &SubWorld, commands: &mut CommandBuffer) {
-    let mut movers = <(Entity, &Point, &ChasingPlayer)>::query();
+    let mut movers = <(Entity, &Point, &FieldOfView)>::query().filter(component::<ChasingPlayer>());
     let mut positions = <(Entity, &Point, &Health)>::query();
     let mut player = <(&Point, &Player)>::query();
 
@@ -16,47 +17,50 @@ pub fn chasing(#[resource] map: &Map, ecs: &SubWorld, commands: &mut CommandBuff
     let search_targets = vec![player_idx];
     let dijkstra_map = DijkstraMap::new(SCREEN_WIDTH, SCREEN_HEIGHT, &search_targets, map, 1024.0);
 
-    movers.iter(ecs).for_each(|(entity, pos, _)| {
-        let idx = map_idx(pos.x, pos.y);
-        if let Some(destination) = DijkstraMap::find_lowest_exit(&dijkstra_map, idx, map) {
-            let distance = DistanceAlg::Pythagoras.distance2d(*pos, *player_pos);
-            let destination = if distance > 1.2 {
-                map.index_to_point2d(destination)
-            } else {
-                *player_pos
-            };
+    movers
+        .iter(ecs)
+        .filter(|(_, _, fov)| fov.visible_tiles.contains(&player_pos))
+        .for_each(|(entity, pos, _)| {
+            let idx = map_idx(pos.x, pos.y);
+            if let Some(destination) = DijkstraMap::find_lowest_exit(&dijkstra_map, idx, map) {
+                let distance = DistanceAlg::Pythagoras.distance2d(*pos, *player_pos);
+                let destination = if distance > 1.2 {
+                    map.index_to_point2d(destination)
+                } else {
+                    *player_pos
+                };
 
-            let mut attacked = false;
-            positions
-                .iter(ecs)
-                .filter(|(_, target_pos, _)| **target_pos == destination)
-                .for_each(|(victim, _, _)| {
-                    if ecs
-                        .entry_ref(*victim)
-                        .unwrap()
-                        .get_component::<Player>()
-                        .is_ok()
-                    {
-                        commands.push((
-                            (),
-                            WantsToAttack {
-                                attacker: *entity,
-                                victim: *victim,
-                            },
-                        ));
-                    }
-                    attacked = true;
-                });
+                let mut attacked = false;
+                positions
+                    .iter(ecs)
+                    .filter(|(_, target_pos, _)| **target_pos == destination)
+                    .for_each(|(victim, _, _)| {
+                        if ecs
+                            .entry_ref(*victim)
+                            .unwrap()
+                            .get_component::<Player>()
+                            .is_ok()
+                        {
+                            commands.push((
+                                (),
+                                WantsToAttack {
+                                    attacker: *entity,
+                                    victim: *victim,
+                                },
+                            ));
+                        }
+                        attacked = true;
+                    });
 
-            if !attacked {
-                commands.push((
-                    (),
-                    WantsToMove {
-                        entity: *entity,
-                        destination,
-                    },
-                ));
+                if !attacked {
+                    commands.push((
+                        (),
+                        WantsToMove {
+                            entity: *entity,
+                            destination,
+                        },
+                    ));
+                }
             }
-        }
-    });
+        });
 }
