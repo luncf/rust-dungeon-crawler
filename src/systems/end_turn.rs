8b53fdc3473630e02a93d7ea3da7e5a1c169f179
Andy Lun <andy.luncf@gmail.com>
2021-11-20 18:58:43 -0600

    Add new dungeon levels

--- src/systems/end_turn.rs
+++ src/systems/end_turn.rs
@@ -5,10 +5,12 @@ use crate::prelude::*;
 #[read_component(Point)]
 #[read_component(Player)]
 #[read_component(AmuletOfYala)]
-pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState) {
+pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState, #[resource] map: &Map) {
     let mut player = <(&Health, &Point)>::query().filter(component::<Player>());
+
     let mut amulet = <&Point>::query().filter(component::<AmuletOfYala>());
-    let amulet_pos = amulet.iter(ecs).nth(0).unwrap();
+    let amulet_default = Point::new(-1, -1);
+    let amulet_pos = amulet.iter(ecs).nth(0).unwrap_or(&amulet_default);
 
     let current_state = turn_state.clone();
     let mut new_state = match current_state {
@@ -21,8 +23,15 @@ pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState) {
     player.iter(ecs).for_each(|(hp, pos)| {
         if hp.current < 1 {
             new_state = TurnState::GameOver;
-        } else if pos == amulet_pos {
+        }
+
+        if pos == amulet_pos {
             new_state = TurnState::Victory;
+        }
+
+        let idx = map.point2d_to_index(*pos);
+        if map.tiles[idx] == TileType::Exit {
+            new_state = TurnState::NextLevel;
         }
     });
 
--- src/systems/hud.rs
+++ src/systems/hud.rs
@@ -11,43 +11,58 @@ pub fn hud(ecs: &SubWorld) {
     draw_batch.target(2);
     draw_batch.print_centered(1, "Explore the dungeon. Cursor keys to move.");
 
-    let mut health_query = <&Health>::query().filter(component::<Player>());
-    let player_health = health_query.iter(ecs).nth(0).unwrap();
+    // Player health
+    {
+        let mut health_query = <&Health>::query().filter(component::<Player>());
+        let player_health = health_query.iter(ecs).nth(0).unwrap();
 
-    draw_batch.bar_horizontal(
-        Point::zero(),
-        SCREEN_WIDTH * 2,
-        player_health.current,
-        player_health.max,
-        ColorPair::new(RED, BLACK),
-    );
-    draw_batch.print_color_centered(
-        0,
-        format!("Health: {} / {}", player_health.current, player_health.max),
-        ColorPair::new(WHITE, RED),
-    );
+        draw_batch.bar_horizontal(
+            Point::zero(),
+            SCREEN_WIDTH * 2,
+            player_health.current,
+            player_health.max,
+            ColorPair::new(RED, BLACK),
+        );
+        draw_batch.print_color_centered(
+            0,
+            format!("Health: {} / {}", player_health.current, player_health.max),
+            ColorPair::new(WHITE, RED),
+        );
+    }
 
-    let player = <(Entity, &Player)>::query()
+    let (player, map_level) = <(Entity, &Player)>::query()
         .iter(ecs)
-        .find_map(|(entity, _player)| Some(*entity))
+        .find_map(|(entity, player)| Some((*entity, player.map_level)))
         .unwrap();
-    let mut y = 3;
-    let mut item_query = <(&Item, &Name, &Carried)>::query();
 
-    item_query
-        .iter(ecs)
-        .filter(|(_, _, carried)| carried.0 == player)
-        .for_each(|(_, name, _)| {
-            draw_batch.print(Point::new(3, y), format!("{} : {}", y - 2, &name.0));
-            y += 1;
-        });
-
-    if y > 3 {
-        draw_batch.print_color(
-            Point::new(3, 2),
-            "Items carried",
+    // Map level
+    {
+        draw_batch.print_color_right(
+            Point::new(SCREEN_WIDTH * 2, 1),
+            format!("Dungeon level: {}", map_level + 1),
             ColorPair::new(YELLOW, BLACK),
         );
+    }
+
+    // Player items
+    {
+        let mut y = 3;
+
+        <(&Item, &Name, &Carried)>::query()
+            .iter(ecs)
+            .filter(|(_, _, carried)| carried.0 == player)
+            .for_each(|(_, name, _)| {
+                draw_batch.print(Point::new(3, y), format!("{} : {}", y - 2, &name.0));
+                y += 1;
+            });
+
+        if y > 3 {
+            draw_batch.print_color(
+                Point::new(3, 2),
+                "Items carried",
+                ColorPair::new(YELLOW, BLACK),
+            );
+        }
     }
 
     draw_batch.submit(10000).expect("Batch error");
